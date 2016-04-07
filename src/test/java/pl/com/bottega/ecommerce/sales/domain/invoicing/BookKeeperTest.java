0932a3ace78d4c8087758715902474f76fe8fe2b
Maciej Sulej <187809@edu.p.lodz.pl>
2016-04-07 18:35:01 +0200

    Test issuance dla jednej pozycji

---
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -0,0 +1,73 @@
+package pl.com.bottega.ecommerce.sales.domain.invoicing;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import org.mockito.Mockito;
+
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+import static org.mockito.Mockito.*;
+
+public class BookKeeperTest {
+    private BookKeeper bookKeeper;
+
+    // Mocks
+    private RequestItem requestItem;
+    private InvoiceRequest invoiceRequest;
+    private TaxPolicy taxPolicy;
+
+    @Before
+    public void setUp() {
+        InvoiceFactory invoiceFactory = new InvoiceFactory();
+        bookKeeper = new BookKeeper(invoiceFactory);
+
+        invoiceRequest = Mockito.mock(InvoiceRequest.class);
+
+        ClientData clientData = mock(ClientData.class);
+        when(invoiceRequest.getClientData()).thenReturn(clientData);
+
+        Money money = new Money(0);
+        Tax tax = new Tax(money, "test");
+
+        ProductData productData = mock(ProductData.class);
+        when(productData.getType()).thenReturn(ProductType.DRUG);
+
+        requestItem = mock(RequestItem.class);
+        when(requestItem.getTotalCost()).thenReturn(money);
+        when(requestItem.getProductData()).thenReturn(productData);
+        when(requestItem.getQuantity()).thenReturn(0);
+
+        taxPolicy = Mockito.mock(TaxPolicy.class);
+        when(taxPolicy.calculateTax(any(ProductType.class), any(Money.class))).
+                thenReturn(tax);
+    }
+
+    @Test
+    public void requestInvoiceMethodWithOneElementShouldReturnInvoiceWithOneElement() {
+        List<RequestItem> requestItemList = getRequestItemList(1, requestItem);
+        when(invoiceRequest.getItems()).thenReturn(requestItemList);
+
+        Invoice invoice = bookKeeper.issuance(invoiceRequest, taxPolicy);
+        assertThat(invoice.getItems().size(), is(1));
+    }
+
+    private static List<RequestItem> getRequestItemList(int size, RequestItem requestItem) {
+        List<RequestItem> requestItemList = new ArrayList<>();
+
+        for (int i = 0; i < size; i++) {
+            requestItemList.add(requestItem);
+        }
+
+        return requestItemList;
+    }
+}
